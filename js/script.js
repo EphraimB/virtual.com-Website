83b717b092c3f04aa1decbfc525232b13f6e2dad
Ephraim Becker <emb16@outlook.com>
2015-10-18 12:44:32 -0400

    Added 2 Apps

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var earpads = document.getElementById("earpads");
+var hub = document.getElementById("hub");
+
+earpads.onclick = function VirtualEarpads()
+{
+alert("Coming soon...");
+};
+
+hub.onclick = function VirtualHub()
+{
+alert("In development");
+};
