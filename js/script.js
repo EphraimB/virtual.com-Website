cda3d2e0667e36ca6a52d4ec56a3d88812ea4739
Ephraim Becker <emb16@outlook.com>
2015-10-18 14:00:22 -0400

    Added a Settings Page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 var earpads = document.getElementById("earpads");
 var hub = document.getElementById("hub");
+var settings = document.getElementById("settings");
+
+function clearPage()
+{
+earpads.style.display = "none";
+hub.style.display = "none";
+settings.style.display = "none";
+};
 
 earpads.onclick = function VirtualEarpads()
 {
@@ -9,4 +17,18 @@ alert("Coming soon...");
 hub.onclick = function VirtualHub()
 {
 alert("In development");
+};
+
+settings.onclick = function Settings()
+{
+document.title = "Virtual-Settings";
+
+clearPage();
+
+var personalizationHeaderHolder = document.createElement("h2");
+
+var personalizationHeader = document.createTextNode("Personalization");
+personalizationHeaderHolder.appendChild(personalizationHeader);
+document.body.appendChild(personalizationHeaderHolder);
+personalizationHeaderHolder.setAttribute("class", "PersonalizationHeader");
 };
